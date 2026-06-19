f9aad6e73bcde1d5aab5c591ff72d3455ddbc826
Mohamed Ismail S <ihssmaheel@gmail.com>
2026-06-19 19:20:57 +0530

    Add  path alias to Vite config for FilterDialog import

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   plugins: [svelte()],
   clearScreen: false,
+  resolve: {
+    alias: {
+      '$lib': path.resolve('./src/lib'),
+    },
+  },
   server: {
     port: 1420,
     strictPort: true,
